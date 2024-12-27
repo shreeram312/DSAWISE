f1495985e7619298de4eba69b3ddccf4ffb8de40
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-12-27 23:44:51 +0530

    dashboard code snippet added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,15 @@
+import Cards from "@/components/dashboard/Cards";
 import Sidebar from "@/components/dashboard/Sidebar";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <div>
+    <div className="flex  ">
+      <div className="">
         <Sidebar />
+      </div>
+      <div className="">
+        <Cards />
       </div>
     </div>
   );
